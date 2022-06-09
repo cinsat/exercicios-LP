ca312f1e4fabc4b6b3450d0ccbf2fb1dd4220d9a
unknown <cintiasatomi@hotmail.com>
2022-06-08 22:52:48 -0300

    Exercicio Prático 2

---
+++ Exercicio2.js
@@ -0,0 +1,5 @@
+const distanciaKm = 100;
+const tempoHoras = 5;
+
+const velocidadeMedia = distanciaKm / tempoHoras;
+console.log(`A velocidade média percorrida por um veículo é de ${velocidadeMedia}km/h`);
